4e229d8a25b3cdb37c68d2416a172e4a78dc5763
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-02 18:59:10 -0400

    commit tree df0853671f6efa953932adc9ef8a1e915fc22df8

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    state = {
+        peopleInSpace: []
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            this.setState({peopleInSpace: data.people})
+        })
+    }
+
+}
